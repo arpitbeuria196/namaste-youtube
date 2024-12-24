ab0ef3f51bf2e708e667447926464ca69e30df4b
Arpit Beuria <arpitbeuria196@gmail.com>
2024-12-24 22:06:32 +0000

    start

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-cyan-200'>
+      Namaste Arpit
+
+
+      {/* {
+      Head
+       Body
+        SideBar
+        MenuItem
+       MainContainer
+        ButtonsList
+        VideoContainer
+          VideoCard
+        
+      } */}
+    </div>
+  );
+}
+
+export default App;
+
+
